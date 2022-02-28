3343e7e6d030d9ecae25188c033dac79e323c51a
vidal james cubero <cubero.vj@gmail.com>
2022-03-01 03:29:07 +0800

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$.getScript( "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js", function(data, textStatus, jqxhr) {
+    
+
+   
+    $('.owl-carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoWidth:true,
+        items: 2,
+        center: true,
+    })
+
+   
+});
+
+
+
